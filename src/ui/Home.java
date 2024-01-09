5f64755083c247b0f84238f20665abfa8f042a64
MehKad <mehkadiri@gmail.com>
2024-01-09 20:09:07 +0000

    see users in home page

--- src/ui/Home.java
+++ src/ui/Home.java
@@ -1,12 +1,20 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.*;
+import java.awt.*;
 
 import users.User;
+import users.UserDAO;
+import users.UserDAOImpl;
 
 public class Home extends JFrame {
 
    private JLabel hello;
+   private JLabel users;
+   private JTextArea usersArea;
 
    public Home(User u) {
       setTitle("Home");
@@ -14,8 +22,29 @@ public class Home extends JFrame {
       setSize(300, 400);
 
       hello = new JLabel("Welcome -- " + u.getUsername());
+      users = new JLabel("Users in our database are : ");
+      usersArea = new JTextArea();
+      usersArea.setEditable(false);
+
+      usersArea.setRows(10);
+      usersArea.setColumns(30);
+
+      List<User> usersDB = new ArrayList<>();
+      UserDAO udao = new UserDAOImpl();
+      usersDB = udao.getUsers();
+
+      StringBuilder usersText = new StringBuilder();
+      for (User i : usersDB) {
+         usersText.append("Username: ").append(i.getUsername()).append("\n");
+      }
+
+      usersArea.setText(usersText.toString());
+
+      setLayout(new GridLayout(6, 2));
 
       add(hello);
+      add(users);
+      add(usersArea);
 
       setVisible(true);
    }
